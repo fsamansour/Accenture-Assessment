a248a9b1050c0e5e66d854130d35fa07b2832cf4
Ahmed Mansour <fs.amansour@gmail.com>
2021-10-27 13:51:43 +0200

    Add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Accenture Assessment
+
+To run all tasks test run the command `npm start`.
+
+to run each task test separately run the command `npm run task[TASK_NUMBER (1,2,4,5)]`.
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const makeNumSentences = require('./task1_makeNumSentences');
-const promises  = require('./task2_promises');
+const promises = require('./task2_promises');
 const getProcessingPage = require('./task4_getProcessingPage');
+const codeReview = require('./task5_codeReview');
 
 makeNumSentences.test();
 
 promises.test();
 
 getProcessingPage.test();
+
+codeReview.test();
